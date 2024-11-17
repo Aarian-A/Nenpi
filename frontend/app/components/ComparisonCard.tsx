b6ab5dfcb44ad4d9018134f2fed5c00a1839e114
WyShadow11 <luujason11@gmail.com>
2024-11-17 06:12:07 -0600

    worked a little on comparsion page

--- frontend/app/components/ComparisonCard.tsx
+++ frontend/app/components/ComparisonCard.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import cardStyles from "../styles/Card.module.css";
+import styles from "../styles/CardComparion.module.css";
+import Image from "next/image";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import SplitButton from "react-bootstrap/SplitButton";
+import { ButtonGroup } from "react-bootstrap";
 
 interface CompCardProps {}
 
@@ -20,3 +25,34 @@ interface CompCardProps {}
 // };
 
 // export default ComparisonCard;
+
+function ComparisonCard() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.red}></div>
+      <div className={styles.white}>
+        <Image
+          src="https://www.motortrend.com/uploads/2021/12/2022-Toyota-Camry-SE-11.jpg"
+          alt="car"
+          sizes="100vw"
+          layout="responsive"
+          width={100}
+          height={100}
+          style={{ borderRadius: "1rem 1rem 0rem 0rem" }}
+        />
+
+        <h1 className={styles.title}>Add first car</h1>
+        <div className={styles.choiceWrapper}></div>
+        <select>
+          <option value="fruit">Fruit</option>
+
+          <option value="vegetable">Vegetable</option>
+
+          <option value="meat">Meat</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ComparisonCard;
--- frontend/app/dashboard/Comparison.tsx
+++ frontend/app/dashboard/Comparison.tsx
@@ -4,31 +4,44 @@ import styles from "../styles/Comparison.module.css";
 import CarCard from "../components/CarCard";
 import cardStyles from "../styles/card.module.css";
 import ScoreCard from "../components/ScoreCard";
+import ComparisonCard from "../components/ComparisonCard";
+
 // import ComparisonCard from "../components/ComparisonCard";
 
 function CompDashboard() {
-  const [cards, setCards] = useState<number[]>([]);
+  // const [cards, setCards] = useState<number[]>([]);
 
-  const addCard = () => {
-    if (cards.length < 4) {
-      setCards([...cards, cards.length + 1]);
-    }
-  };
+  // const addCard = () => {
+  //   if (cards.length < 4) {
+  //     setCards([...cards, cards.length + 1]);
+  //   }
+  // };
 
-  const searchCar = (event: { target: { value: string } }) => {
-    console.log(event.target.value);
-  };
+  // const searchCar = (event: { target: { value: string } }) => {
+  //   console.log(event.target.value);
+  // };
 
   return (
+    // <div className={styles.main}>
+    //   <input type="text" placeholder="Enter Car Name" onChange={searchCar} />
+    //   <button onClick={addCard}>Add Comparison Card</button>
+    //   <div className={styles.gridContainer}>
+    //     {/* {cards.map((card, index) => (
+    //     //   <ComparisonCard
+    //     //     key={index}
+    //     //     />
+    //     ))} */}
+    //   </div>
+    // </div>
+
     <div className={styles.main}>
-      <input type="text" placeholder="Enter Car Name" onChange={searchCar} />
-      <button onClick={addCard}>Add Comparison Card</button>
-      <div className={styles.gridContainer}>
-        {/* {cards.map((card, index) => (
-        //   <ComparisonCard 
-        //     key={index}
-        //     />
-        ))} */}
+      <h1 className={styles.title}>Compare cars</h1>
+      <p className={styles.subheading}>
+        Choose two cars to compare side-by-side
+      </p>
+      <div className={styles.carCardWrapper}>
+        <ComparisonCard />
+        <ComparisonCard />
       </div>
     </div>
   );
