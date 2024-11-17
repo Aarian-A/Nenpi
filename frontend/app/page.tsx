1c7c97114a58d5a895828ef4b6ea512e9392c7cc
Abel Thomas <109200318+Sharktail001@users.noreply.github.com>
2024-11-17 00:25:42 -0600

    navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,10 +4,14 @@ import styles from "../app/styles/page.module.css";
 import React, { useState, useEffect } from "react";
 import { GoPaperclip } from "react-icons/go";
 import { IoIosSend } from "react-icons/io";
+import NavBar from "./components/Navbar";
+
 
 const NenpiComponent = () => {
   const [showSearchBar, setShowSearchBar] = useState(false);
   const [search, setSearch] = useState("");
+
+  let items = ["Home","Product", "Service"];
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -23,22 +27,27 @@ const NenpiComponent = () => {
   };
 
   return (
-    <div className={`${styles.main}`}>
-      <h1 className={`${styles.title} ${styles.fadeOut}`}>Nenpi</h1>
-      <div
-        className={`${styles.searchBar} ${
-          showSearchBar ? styles.searchBarVisible : ""
-        }`}
-      >
-        <GoPaperclip size={"1.75rem"} />
-        <input
-          type="text"
-          className={styles.input}
-          placeholder="Message Nenpi"
-          onChange={handleChange}
-        />
-        <div className={styles.icon}>
-          <IoIosSend size={"1.75rem"} />
+    <div>
+      <NavBar 
+          brandName="My Brand"
+          navItems={items}/>
+      <div className={`${styles.main}`}>
+        <h1 className={`${styles.title} ${styles.fadeOut}`}>Nenpi</h1>
+        <div
+          className={`${styles.searchBar} ${
+            showSearchBar ? styles.searchBarVisible : ""
+          }`}
+        >
+          <GoPaperclip size={"1.75rem"} />
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Message Nenpi"
+            onChange={handleChange}
+          />
+          <div className={styles.icon}>
+            <IoIosSend size={"1.75rem"} />
+          </div>
         </div>
       </div>
     </div>
